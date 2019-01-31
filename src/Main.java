79abc8824632e2d15ea76c68c5dd79cbef444fd7
br17011 <br17011@essex.ac.uk>
2019-01-31 15:44:07 +0000

    Gets the colour of the pixel that the mouse is on and returns it as an int[r,g,b]

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.css.RGBColor;
+
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -6,8 +8,23 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws AWTException, IOException {
+        Point location = MouseInfo.getPointerInfo().getLocation();
+        System.out.println(location.toString());
+
         Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
         BufferedImage capture = new Robot().createScreenCapture(screenRect);
-        ImageIO.write(capture, "bmp", new File("Yes.bmp"));
+
+        int c = capture.getRGB(location.x, location.y);
+        int[] RGB = RGBconvert(c);
+
+        System.out.println(RGB[0] + ", " + RGB[1] + ", " + RGB[2]);
+    }
+
+    static int[] RGBconvert(int c){
+        int red = (c >> 16) & 0xff;
+        int green = (c >> 8) & 0xff;
+        int blue = (c) & 0xff;
+        System.out.println("R: " + red + ", G: " + green + ", B: " + blue);
+        return new int[]{red,green,blue};
     }
 }
