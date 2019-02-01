8776f1896405ee5bbac108a7ab0dc1ce63e782b2
Ben <br17011@essex.ac.uk>
2019-02-01 01:47:14 +0000

    Added hexadecimal conversion

--- src/grabberColour.java
+++ src/grabberColour.java
@@ -9,12 +9,40 @@ public class grabberColour {
         blue = (pixel) & 0xff;
         hex = convertToHex(red, green, blue);
 
-        System.out.println("R: " + red + ", G: " + green + ", B: " + blue);
+        System.out.println("R: " + red + ", G: " + green + ", B: " + blue + ", " + hex);
     }
 
     String convertToHex(int red, int green, int blue){
-        String out = "#";
+        StringBuilder out = new StringBuilder("#");
+        int[] color = {red, green, blue};
 
-        return out;
+        for (int c: color){
+            int opperation = c/16;
+            for (int i = 0; i < 2; i++){
+                if (opperation == 10) {
+                    out.append("A");
+                }
+                else if (opperation == 11) {
+                    out.append("B");
+                }
+                else if (opperation == 12) {
+                    out.append("C");
+                }
+                else if (opperation == 13) {
+                    out.append("D");
+                }
+                else if (opperation == 14) {
+                    out.append("E");
+                }
+                else if (opperation == 15) {
+                    out.append("F");
+                }
+                else{
+                    out.append(opperation);
+                }
+                opperation = c%16;
+            }
+        }
+        return out.toString();
     }
 }
